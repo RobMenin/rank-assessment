eb8ef6e664f2973df5329543af7f962262c4f0f3
RobMenin <robertmenin2@gmail.com>
2023-08-17 15:04:21 +0200

    feat(general): added new images, page routes and dependecies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
 import Cards from "./CardManager/Cards/Cards";
+import AddCard from "./CardManager/AddCard/AddCard";
+import EditCard from "./CardManager/EditCard/EditCard";
+import BannedCountries from "./CardManager/BannedCountries/BannedCountries";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Cards />} />
+          <Route path="/add-card" element={<AddCard />} />
+          <Route path="/banned-countries" element={<BannedCountries />} />
+          <Route path="/cards/:id/edit" element={<EditCard />} />
         </Routes>
       </BrowserRouter>
     </>
